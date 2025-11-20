9940eb04940c99587ad63d060509a7b5a7e7fa06
Rodrigo Zordán <zordan.rodrigo97@gmail.com>
2025-11-20 15:21:08 -0300

    checkWifiCredentialsBySsid

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -60,5 +60,5 @@ export interface WifiPlugin {
     checkPermissions(): Promise<PermissionStatus>;
     requestPermissions(): Promise<PermissionStatus>;
     disconnectAndForget(): Promise<void>;
-    checkWifiCredentialsBySsid(): Promise<ConnectToWifiResult>;
+    checkWifiCredentialsBySsid(connectToWifiRequest: ConnectToWifiRequest): Promise<ConnectToWifiResult>;
 }
